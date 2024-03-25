f841bfb64c29992683275543372e6ef6cd7e6bd2
Meliodas98765 <dragonheart98765@gmail.com>
2024-03-26 01:47:12 +0530

    Webhook Nginx setup test

--- peak.py
+++ peak.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    mountains = ['Everest', 'K2', 'Kilimanjaro','Kailash','Fuji']
+    mountains = ['Everest', 'K2', 'Kilimanjaro','Kailash','Fuji','Himalayas']
     return render_template('index.html', mountain=mountains)
 
 @app.route('/mountain/<mt>')
