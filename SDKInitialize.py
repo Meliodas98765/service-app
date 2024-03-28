615777a5b9e11e185ac29f79c0cbfadf0abd28ce
Meliodas98765 <dragonheart98765@gmail.com>
2024-03-29 02:59:17 +0530

    Updated status logging in SDKInitialize

--- SDKInitialize.py
+++ SDKInitialize.py
@@ -3,7 +3,7 @@ from zcrmsdk.src.com.zoho.crm.api.dc import USDataCenter, INDataCenter
 from zcrmsdk.src.com.zoho.api.authenticator.store import DBStore, FileStore
 from zcrmsdk.src.com.zoho.api.logger import Logger
 from zcrmsdk.src.com.zoho.crm.api.initializer import Initializer
-from zcrmsdk.src.com.zoho.api.authenticator.oauth_token import OAuthToken
+from zcrmsdk.src.com.zoho.api.authenticator.oauth_token import OAuthToken,TokenType
 from zcrmsdk.src.com.zoho.crm.api.sdk_config import SDKConfig
 import os
 from dotenv import load_dotenv
@@ -16,7 +16,7 @@ client_secret = os.getenv("CLIENT_SECRET")
 redirect_url = os.getenv("REDIRECT_URL")
 refresh_token = os.getenv("REFRESH_TOKEN")
 user_email = os.getenv("USER_EMAIL")
-
+print(redirect_url)
 class SDKInitializer(object):
 
     @staticmethod
@@ -25,9 +25,18 @@ class SDKInitializer(object):
         user = UserSignature(email=user_email)
         environment = INDataCenter.PRODUCTION()
         # token = OAuthToken(client_id=client_id, client_secret=client_secret, grant_token='grant_token', refresh_token="refresh_token", redirect_url='redirectURL', id="id")
-        token = OAuthToken(client_id=client_id, client_secret=client_secret, redirect_url="https://www.google.com",refresh_token=refresh_token)
+        try:
+            token = OAuthToken(client_id=client_id, client_secret=client_secret, redirect_url="https://www.google.com",refresh_token=refresh_token)
+        except TypeError:
+            # Different lib version 
+            token = OAuthToken(client_id=client_id, client_secret=client_secret, redirect_url="https://www.google.com",token=refresh_token,token_type=TokenType.REFRESH)
         store = FileStore(file_path='./python_sdk_tokens.txt')
-        config = SDKConfig(auto_refresh_fields=True, pick_list_validation=False, connect_timeout=None, read_timeout=None)
+        try:
+            config = SDKConfig(auto_refresh_fields=True, pick_list_validation=False, connect_timeout=None, read_timeout=None)
+        except TypeError:
+            # Different lib version 
+            config = SDKConfig(auto_refresh_fields=True, pick_list_validation=False)
+        
         resource_path = r'./'
         Initializer.initialize(user=user, environment=environment, token=token, store=store, sdk_config=config, resource_path=resource_path)
 
