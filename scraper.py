0457dc6a26a65ff9ee720bf137c7559e5bab55cc
Meliodas98765 <dragonheart98765@gmail.com>
2024-03-29 03:01:07 +0530

    Updated status logging and execption handle for browser instance launch in scraper

--- scraper.py
+++ scraper.py
@@ -4,11 +4,32 @@ from pyppeteer_stealth import stealth
 from pyppeteer.browser import Browser
 
 # Batch of 10 reads and change proxy
-async def run_scraper(url_list):
+async def run_scraper(url_list,batch_id):
+    browser = None
+    try:
+        update_status(batch_id,"Launching Browser..","DESCRIPTION")
+        browser = await launch({
+            'headless': True,
+        })
+    except RuntimeError as e:
+        update_status(batch_id,"Failed in sandbox mode","FAILED")
+        if (str(e) == "Event loop is closed"):
+            # inside server env
+            update_status(batch_id,"Launching Browser in Production env..","DESCRIPTION")
+            browser = await launch({
+                'headless': True,
+                'args': ['--no-sandbox'],
+            })
+            execute_url_scrapping(url_list,batch_id)
+    except Exception as e:
+        if ("Browser closed unexpectedly" in str(e)):
+        update_status(batch_id,"Failed launching Browser","FAILED")
+    finally:
+        if browser is not None:
+            await browser.close()
 
-    browser = await launch({
-        'headless': True,
-    })
+def execute_url_scrapping(url_list,batch_id):
+    update_status(batch_id,"Launch Successful!!","DESCRIPTION")
     page = await browser.newPage()
 
     await stealth(page)
@@ -40,6 +61,28 @@ async def run_scraper(url_list):
 
         await asyncio.sleep(random.uniform(20, 60))
 
-    await browser.close()  # Close the browser
+def update_status(batch_id,log,status):
+    print("BATCH_ID : "+batch_id + "\n LOG : " + log + "\n STATUS : " +status )
+    if not os.path.exists("status.json"):
+        # Create an empty JSON object if the file doesn't exist
+        status_data = {"batch_list": {}}
+    else:
+        # Read the existing JSON data from the file
+        with open("status.json","r+") as file:
+            try:
+                status_data = json.load(file)
+            except json.JSONDecodeError:
+                status_data = {"batch_list": {}}
+    status_data["batch_list"].setdefault(batch_id, {"logs": [], "status": ""})
+    if status == "DESCRIPTION":
+
+        status_data["batch_list"][batch_id]["logs"].append(status +" - "+ log)
+    else:
+        status_data["batch_list"][batch_id]["logs"].append(status +" - "+ log)
+        if status_data["batch_list"][batch_id]["status"] != status:
+            status_data["batch_list"][batch_id]["status"] = status
+
+    with open("status.json", "w") as file:
+        json.dump(status_data, file, indent=2)
 
 # asyncio.get_event_loop().run_until_complete(run_scraper())
