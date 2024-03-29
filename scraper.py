c088a29572137677fea74563953edea64af6b6c2
Meliodas98765 <dragonheart98765@gmail.com>
2024-03-29 21:58:01 +0530

    Bugfix in scraper

--- scraper.py
+++ scraper.py
@@ -9,46 +9,44 @@ logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %
 # Batch of 10 reads and change proxy
 async def run_scraper(url_list,batch_id):
     logging.info("Starting Scraper")
-    try:
-        browser = await launch({
-            'headless': True,
-            'args': ['--no-sandbox'],
-        })
-        logging.info("Browser Initiated")
-        page = await browser.newPage()
-        logging.info("Browser Open New Page")
-        await stealth(page)
-        await page.goto('https://www.linkedin.com/login')
-        logging.info("Browser Open Login Page")
-        await page.type('div.form__input--floating input#username', 'mintu.test98@gmail.com')
-        await page.type('div.form__input--floating input#password', 'Qwertyasdf#221')
-        await page.click('div.login__form_action_container > button', waitUntil='networkidle0')
-        logging.info("Sign In Successfull")
-        await asyncio.sleep(random.uniform(5, 15))
+    
+    browser = await launch({
+        'headless': True,
+        'args': ['--no-sandbox','--disable-setuid-sandbox','--disable-dev-shm-usage'],
+    })
+    logging.info("Browser Initiated")
+    page = await browser.newPage()
+    logging.info("Browser Open New Page")
+    await stealth(page)
+    await page.goto('https://www.linkedin.com/login')
+    logging.info("Browser Open Login Page")
+    await page.type('div.form__input--floating input#username', 'mintu.test98@gmail.com')
+    await page.type('div.form__input--floating input#password', 'Qwertyasdf#221')
+    await page.click('div.login__form_action_container > button', waitUntil='networkidle0')
+    logging.info("Sign In Successfull")
+    await asyncio.sleep(random.uniform(5, 15))
 
-        for go_to_url in url_list:
+    for go_to_url in url_list:
 
-        # go_to_url = "https://www.linkedin.com/in/theharishnarayanan/"
-            # print(go_to_url)
-            file_path = "sources/"+go_to_url.split("https://www.linkedin.com/in/")[1].replace("/","") + ".html"
-            if not os.path.exists(file_path):  # Temporary
-                await page.goto(go_to_url)
+    # go_to_url = "https://www.linkedin.com/in/theharishnarayanan/"
+        # print(go_to_url)
+        file_path = "sources/"+go_to_url.split("https://www.linkedin.com/in/")[1].replace("/","") + ".html"
+        if not os.path.exists(file_path):  # Temporary
+            await page.goto(go_to_url)
 
-                await asyncio.sleep(random.uniform(15, 35))
+            await asyncio.sleep(random.uniform(15, 35))
 
-                page_source = await page.content()
+            page_source = await page.content()
 
-                os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                with open(file_path, 'w', encoding='utf-8') as file:
-                    file.write(page_source)
-            else: # Temporary
-                print(f"File {file_path} already exists. Skipping writing page content.")
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(page_source)
+        else: # Temporary
+            print(f"File {file_path} already exists. Skipping writing page content.")
 
-            await asyncio.sleep(random.uniform(20, 60))
+        await asyncio.sleep(random.uniform(20, 60))
 
-        await browser.close()  # Close the browser
-    except Exception as e:
-        logging.error("Error occured : {e}")
+    await browser.close()  # Close the browser
     
 
     # try:
